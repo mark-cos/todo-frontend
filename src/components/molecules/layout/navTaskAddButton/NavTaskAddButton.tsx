4fc44664f5922802bd54cb913b461209116c8df2
devHong <choisieun89@gmail.com>
2024-02-01 20:23:19 +0900

    refactor: remove redux

--- src/components/molecules/layout/navTaskAddButton/NavTaskAddButton.tsx
+++ src/components/molecules/layout/navTaskAddButton/NavTaskAddButton.tsx
@@ -3,9 +3,9 @@
 import Image from 'next/image';
 import React from 'react';
 import addIcon from '@/images/icons/add.svg?url';
-import { initialTaskState } from '@/libs/redux/slices/taskSlice';
 import { TASK_FORM_STEP } from '@/types/task/task.type';
 import { taskStore } from '@/libs/zustand';
+import { initialTaskState } from '@/libs/zustand/slices/taskSlice';
 
 const NavTaskAddButton = () => {
   const { isEditMode, setTaskFormStep, setTask, setIsShowModal } = taskStore(
@@ -14,7 +14,7 @@ const NavTaskAddButton = () => {
 
   /**
    * task 추가버튼 이벤트
-   * 수정화면에서는 추가가 불가능(현재 구조에서 모달과 redux store 같이 사용하므로)
+   * 수정화면에서는 추가가 불가능(현재 구조에서 모달과  store 같이 사용하므로)
    * state초기화 및 모달 단계 메인으로 설정
    */
   const handleClickNewTask = () => {
