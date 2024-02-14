883dd0238aaa421c1df6ebbd748500d395fc3d75
devHong <choisieun89@gmail.com>
2024-02-14 21:50:51 +0900

    wip: intersection-observer

--- src/components/organisms/intro/Intro1.tsx
+++ src/components/organisms/intro/Intro1.tsx
@@ -1,31 +1,32 @@
+'use client';
 import Image from 'next/image';
-import React from 'react';
+import React, { ForwardedRef, forwardRef } from 'react';
 import intro1Img from '@/images/intro1.svg?url';
-import useServerTranslation from '@/libs/i18n/useServerTranslation';
-import ROUTE from '@/libs/route';
-import Link from '@/components/atoms/link/Link';
-import Button from '@/components/atoms/button/Button';
 
-const Intro1 = async () => {
-  const { t } = await useServerTranslation('intro');
+import Button from '@/components/atoms/button/Button';
+import { useClientTranslation } from '@/libs/i18n/useClientTranslation';
+
+const Intro1 = forwardRef(function Intro1({}, ref: ForwardedRef<HTMLDivElement>) {
+  console.log('🚀 ~ Intro1 ~ ref:', ref);
+  const { t } = useClientTranslation('intro');
 
   return (
-    <div className="container-100svh flex-col items-center justify-center">
+    <div className="container-100svh flex-col items-center justify-center" ref={ref}>
       <Image src={intro1Img} alt="mainLogo" priority />
       <div className="absolute bottom-11 w-full flex-auto px-5">
-        <Link href={ROUTE.ACCOUNT.LOGIN.path}>
-          <Button variant="contained" className="mb-5 w-full">
-            {t('button.login')}
-          </Button>
-        </Link>
-        <Link href={ROUTE.ACCOUNT.REGISTER.path}>
-          <Button variant="outlined" className="w-full">
-            {t('button.create_account')}
-          </Button>
-        </Link>
+        {/* <Link href={ROUTE.ACCOUNT.LOGIN.path}> */}
+        <Button variant="contained" className="mb-5 w-full">
+          {t('button.login')}
+        </Button>
+        {/* </Link> */}
+        {/* <Link href={ROUTE.ACCOUNT.REGISTER.path}> */}
+        <Button variant="outlined" className="w-full">
+          {t('button.create_account')}
+        </Button>
+        {/* </Link> */}
       </div>
     </div>
   );
-};
+});
 
 export default Intro1;
--- src/components/templates/intro/IntroTempl.tsx
+++ src/components/templates/intro/IntroTempl.tsx
@@ -1,10 +1,18 @@
-import { Intro1, Intro2, Intro3, Intro4 } from '@/components/organisms';
-import React from 'react';
+'use client';
+
+import Intro1 from '@/components/organisms/intro/Intro1';
+import Intro2 from '@/components/organisms/intro/Intro2';
+import Intro3 from '@/components/organisms/intro/Intro3';
+import Intro4 from '@/components/organisms/intro/Intro4';
+import React, { useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 const IntroTempl = () => {
+  const [ref, inView, entry] = useInView({});
+
   return (
     <>
-      <Intro1 />
+      <Intro1 ref={ref} />
       <Intro2 />
       <Intro3 />
       <Intro4 />
