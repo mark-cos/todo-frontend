7d6172bb009534a5199083ed5f586d33de2e1abb
dev-HongCheol <choisieun89@gmail.com>
2024-03-17 21:08:20 +0900

    feat: seo 관련 설정 및 파일 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -69,5 +69,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|robots).*)'],
 };
