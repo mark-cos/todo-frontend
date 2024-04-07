aec987d6d8e652ff7ac085d232eb2887fb7cf460
dev-HongCheol <choisieun89@gmail.com>
2024-04-08 01:00:17 +0900

    Wip: task 상단 UI import 위치 변경

--- src/components/templates/tasks/TasksPageTempl.tsx
+++ src/components/templates/tasks/TasksPageTempl.tsx
@@ -2,13 +2,23 @@
 import React, { Suspense } from 'react';
 import TodayTaskPage from './TodayTaskPage';
 import TaskListSkeleton from '@/components/organisms/taskList/TaskList.skeleton';
+import TaskSearchInput from '@/components/organisms/taskSearchInput/TaskSearchInput';
+import TaskFilter from '@/components/organisms/taskFilter/TaskFilter';
 
 const TasksPageTempl = () => {
   return (
     <div className="flex flex-col">
-      <Suspense fallback={<TaskListSkeleton />}>
-        <TodayTaskPage />
-      </Suspense>
+      <div className="flex-none">
+        <TaskSearchInput />
+      </div>
+      <div className="mt-4">
+        <div className="mt-4">
+          <TaskFilter />
+        </div>
+        <Suspense fallback={<TaskListSkeleton />}>
+          <TodayTaskPage />
+        </Suspense>
+      </div>
     </div>
   );
 };
--- src/components/templates/tasks/TodayTaskPage.tsx
+++ src/components/templates/tasks/TodayTaskPage.tsx
@@ -1,23 +1,11 @@
 import TaskList from '@/components/organisms/taskList/TaskList';
 import React from 'react';
 import useTodayTaskPage from './TodayTaskPage.hook';
-import TaskSearchInput from '@/components/organisms/taskSearchInput/TaskSearchInput';
-import TaskFilter from '@/components/organisms/taskFilter/TaskFilter';
 import TodayTaskNone from '@/components/molecules/todayTaskNone/TodayTaskNone';
 
 const TodayTaskPage = () => {
   const { tasks, isTodayNone } = useTodayTaskPage();
-
-  return (
-    <>
-      <div className="flex-none">
-        <TaskSearchInput />
-      </div>
-      <div className="mt-4">
-        <TaskFilter />
-        {tasks ? <TaskList tasks={tasks} /> : <TodayTaskNone />}
-      </div>
-    </>
-  );
+  console.log('🚀 _ TodayTaskPage _ tasks:', tasks);
+  return <>{tasks.length !== 0 ? <TaskList tasks={tasks} /> : <TodayTaskNone />}</>;
 };
 export default TodayTaskPage;
